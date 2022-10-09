3bb6dcc7f71f91e1bc7b7019984756d8ebed1b5d
Dmitriy Goron <pitbull9846@gmail.com>
2022-10-10 01:04:45 +0300

    FiboB is done

--- src/by/it/group151051/goron/lesson01/FiboB.java
+++ src/by/it/group151051/goron/lesson01/FiboB.java
@@ -25,7 +25,14 @@ public class FiboB {
 
     BigInteger fastB(Integer n) {
         //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
-        return BigInteger.ZERO;
+        BigInteger[] cache = new BigInteger[n+1]; //Массив вычисленных чисел
+        cache[0] = BigInteger.ZERO;
+        cache[1] = BigInteger.ONE;
+
+        for (int i = 2; i <= n; i++) {
+            cache[i] = cache[i-1].add(cache[i-2]);
+        }
+        return cache[n];
     }
 
 }
