5a3ec05cf177147f01e122db18201b14fef460db
Dmitriy Goron <pitbull9846@gmail.com>
2022-11-17 23:59:22 +0300

    Lesson 3 A is done

--- src/by/it/group151051/goron/lesson03/A_Huffman.java
+++ src/by/it/group151051/goron/lesson03/A_Huffman.java
@@ -116,25 +116,41 @@ public class A_Huffman {
         Scanner scanner = new Scanner(file);
         String s = scanner.next();
 
-        //все комментарии от тестового решения были оставлены т.к. это задание A.
-        //если они вам мешают их можно удалить
-
-        Map<Character, Integer> count = new HashMap<>();
-        //1. переберем все символы по очереди и рассчитаем их частоту в Map count
-            //для каждого символа добавим 1 если его в карте еще нет или инкремент если есть.
+        //1. переберем все символы по очереди и рассчитаем их частоту в freqMap
+        //для каждого символа добавим 1 если его в карте еще нет или инкремент если есть.
+        Map<Character, Integer> freqMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            Integer count = freqMap.get(c);
+            freqMap.put(c, count != null ? count + 1 : 1);
+        }
 
         //2. перенесем все символы в приоритетную очередь в виде листьев
         PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        for (Character c : freqMap.keySet()) {
+            priorityQueue.add(new LeafNode(freqMap.get(c), c));
+        }
 
         //3. вынимая по два узла из очереди (для сборки родителя)
         //и возвращая этого родителя обратно в очередь
         //построим дерево кодирования Хаффмана.
         //У родителя частоты детей складываются.
+        while (priorityQueue.size() > 1) {
+            Node left = priorityQueue.remove();
+            Node right = priorityQueue.remove();
+            InternalNode parent = new InternalNode (left, right);
+            priorityQueue.add (parent);
+        }
 
         //4. последний из родителей будет корнем этого дерева
         //это будет последний и единственный элемент оставшийся в очереди priorityQueue.
+        Node root = priorityQueue.remove();
+        root.fillCodes("");
+
         StringBuilder sb = new StringBuilder();
-        //.....
+        for (char c : s.toCharArray()) {
+            sb.append(codes.get(c));
+        }
 
         return sb.toString();
         //01001100100111
