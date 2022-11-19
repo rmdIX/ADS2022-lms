42bbc47a8b8a8336299d512603bc4f59766d0c50
Dmitriy Goron <pitbull9846@gmail.com>
2022-11-19 03:10:58 +0300

    Lesson 3 B is done

--- src/by/it/group151051/goron/lesson03/B_Huffman.java
+++ src/by/it/group151051/goron/lesson03/B_Huffman.java
@@ -2,6 +2,8 @@ package by.it.group151051.goron.lesson03;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 // Lesson 3. B_Huffman.
@@ -20,7 +22,7 @@ import java.util.Scanner;
 // Исходная строка и коды всех букв непусты.
 // Заданный код таков, что закодированная строка имеет минимальный возможный размер.
 //
-//        Sample Input 1:
+//        Sample Input11101 1:
 //        1 1
 //        a: 0
 //        0
@@ -43,16 +45,37 @@ import java.util.Scanner;
 public class B_Huffman {
 
     String decode(File file) throws FileNotFoundException {
-        StringBuilder result=new StringBuilder();
+        StringBuilder result;
         //прочитаем строку для кодирования из тестового файла
         Scanner scanner = new Scanner(file);
         Integer count = scanner.nextInt();
         Integer length = scanner.nextInt();
+
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! НАЧАЛО ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-        //тут запишите ваше решение
+        //Получим символы и их коды
+        Map<String, String> codes = new HashMap<>();
+        scanner.useDelimiter(": |\\p{javaWhitespace}+"); // Дополнительно в кач-ве разделителя используется ": "
+        for (int i = 0; i < count; i++) {
+            String value = scanner.next();
+            String key = scanner.next();
+            codes.put(key, value);
+        }
 
+        //Получим закодированную строку
+        String encodedStr = scanner.next();
 
-
+        StringBuilder decodedStr = new StringBuilder();
+        String currentCode = "";
+        char currentChar;
+        for (int i = 0; i < length; i++) {
+            currentChar = encodedStr.charAt(i); // Получаем символ закодированной строки
+            currentCode += currentChar; // Добавляем его к текущему коду
+            if (codes.containsKey(currentCode)) { // Если такой код есть в HashMap
+                decodedStr.append(codes.get(currentCode)); // Получаем букву с таким кодом
+                currentCode = "";
+            }
+        }
+        result = decodedStr;
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! КОНЕЦ ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
         return result.toString(); //01001100100111
@@ -60,11 +83,9 @@ public class B_Huffman {
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        File f = new File(root + "by/it/a_khmelev/lesson03/encodeHuffman.txt");
+        File f = new File(root + "by/it/group151051/goron/lesson03/encodeHuffman.txt");
         B_Huffman instance = new B_Huffman();
         String result = instance.decode(f);
         System.out.println(result);
     }
-
-
 }
