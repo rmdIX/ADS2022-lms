e54fefba1c96abe67e4fe12dfb1e858eaca473ef
Dmitriy Goron <pitbull9846@gmail.com>
2022-11-20 18:57:23 +0300

    Lesson 4 C is done

--- src/by/it/group151051/goron/lesson04/C_GetInversions.java
+++ src/by/it/group151051/goron/lesson04/C_GetInversions.java
@@ -35,6 +35,56 @@ Sample Output:
 
 public class C_GetInversions {
 
+    public static int merge_inv_cnt(int[] arr, int[] aux, int low, int midInd, int high) {
+        int auxInd = low, leftInd = low, rightInd = midInd + 1;
+        int inversionCount = 0;
+
+        // Пока есть элементы в левой и правой части массива
+        while (leftInd <= midInd && rightInd <= high) {
+            if (arr[leftInd] <= arr[rightInd]) {
+                aux[auxInd++] = arr[leftInd++];
+            }
+            else {
+                aux[auxInd++] = arr[rightInd++];
+                // Т.к. левая часть массива отсортирована, то все её элементы после arr[leftInd],
+                // а также центральный элемент образуют инверсии с элементом arr[rightInd] правой части
+                inversionCount += (midInd - leftInd + 1);
+            }
+        }
+
+        // Копируем оставшиеся элементы левой части, если в правой части элементов нет
+        while (leftInd <= midInd) {
+            aux[auxInd++] = arr[leftInd++];
+        }
+
+        // Отразим отсортированный подмассив в основном массиве
+        for (auxInd = low; auxInd <= high; auxInd++) {
+            arr[auxInd] = aux[auxInd];
+        }
+
+        return inversionCount;
+    }
+
+    public static int mergesort(int[] arr, int[] aux, int low, int high) {
+        if (high <= low) {  // Если размер части <= 1
+            return 0;
+        }
+
+        // Поиск центрального элемента
+        int mid = (low + high) >>> 1;
+        int inversionCount = 0;
+
+        // Разделить/объединить левую часть
+        inversionCount += mergesort(arr, aux, low, mid);
+        // Разделить/объединить правую часть
+        inversionCount += mergesort(arr, aux, mid + 1, high);
+
+        // Затем половины объединяются и подсчитываются инверсии
+        inversionCount += merge_inv_cnt(arr, aux, low, mid, high);
+
+        return inversionCount;
+    }
+
     int calc(InputStream stream) throws FileNotFoundException {
         //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
@@ -46,25 +96,16 @@ public class C_GetInversions {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        int result = 0;
         //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-
-
-
-
-
-
+        int[] aux = a.clone();
+        return mergesort(a, aux, 0, a.length - 1);
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
     }
 
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataC.txt");
+        InputStream stream = new FileInputStream(root + "by/it/group151051/goron/lesson04/dataC.txt");
         C_GetInversions instance = new C_GetInversions();
         //long startTime = System.currentTimeMillis();
         int result = instance.calc(stream);
