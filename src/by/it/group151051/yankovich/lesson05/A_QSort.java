39ffacbfaeb8133232fb6b50c7463593c859254c
Golovolastik <yankovich.aliaksei@gmail.com>
2022-12-22 19:32:15 +0300

    compare method

--- src/by/it/group151051/yankovich/lesson05/A_QSort.java
+++ src/by/it/group151051/yankovich/lesson05/A_QSort.java
@@ -58,7 +58,21 @@ public class A_QSort {
         @Override
         public int compareTo(Segment o) {
             //подумайте, что должен возвращать компаратор отрезков
-
+            if(start < o.start){
+                return -1;
+            }
+            else if(start > o.start){
+                return 1;
+            }
+            else if(start==o.start){
+                if(stop < o.stop){
+                    return -1;
+                }
+                else if(stop > o.stop){
+                    return 1;
+                }
+                else return 0;
+            }
             return 0;
         }
     }
