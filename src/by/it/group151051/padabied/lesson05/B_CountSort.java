9d18afe839e0da68fe4c0620064520cd7d04c093
Padabied <Padabied@gmail.com>
2023-01-01 17:48:22 +0300

    Lesson 5, B

--- src/by/it/group151051/padabied/lesson05/B_CountSort.java
+++ src/by/it/group151051/padabied/lesson05/B_CountSort.java
@@ -3,6 +3,7 @@ package by.it.group151051.padabied.lesson05;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -18,26 +19,57 @@ import java.util.Scanner;
 public class B_CountSort {
 
 
-    int[] countSort(InputStream stream) throws FileNotFoundException {
-        //подготовка к чтению данных
+    int[] countSort(InputStream stream) {
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        //размер массива
         int n = scanner.nextInt();
         int[] points=new int[n];
-
-        //читаем точки
         for (int i = 0; i < n; i++) {
             points[i]=scanner.nextInt();
         }
-        //тут реализуйте логику задачи с применением сортировки подсчетом
 
+        int min = points[0];
+        int max = points[0];
 
+        for (int i = 1; i < points.length; i++) {
+            if (points[i] < min) {
+                min = points[i];
+            }
+            if (points[i] > max) {
+                max = points[i];
+            }
+        }
+        return sort(points, min, max);
+    }
 
+    int[] sort(int[] array, int min, int max) {
+        int[] freq = new int[max - min + 1];
+        int[] result = new int[max - min + 1];
+        Arrays.fill(freq, 0);
 
-
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return points;
+        for (int i = 0; i < array.length; i++) {
+            int curr = array[i];
+            int cnt = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == curr) {
+                    cnt++;
+                }
+            }
+            freq[curr] = cnt;
+        }
+        StringBuilder arr = new StringBuilder();
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] > 0) {
+                for (int j = 0; j < freq[i]; j++) {
+                    arr.append(i);
+                }
+            }
+        }
+        int index = 0;
+        for (char x : arr.toString().toCharArray()) {
+            result[index] = Integer.parseInt(String.valueOf(x));
+            index++;
+        }
+        return result;
     }
 
 
