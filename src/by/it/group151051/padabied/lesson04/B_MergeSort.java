aa51948d588d30100cff79cdb415971009a00a79
Padabied <Padabied@gmail.com>
2023-01-01 16:57:35 +0300

    Lesson 4, B

--- src/by/it/group151051/padabied/lesson04/B_MergeSort.java
+++ src/by/it/group151051/padabied/lesson04/B_MergeSort.java
@@ -21,31 +21,76 @@ Sample Output:
 */
 public class B_MergeSort {
 
-    int[] getMergeSort(InputStream stream) throws FileNotFoundException {
-        //подготовка к чтению данных
+    int[] getMergeSort(InputStream stream)  {
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-
-        //размер массива
         int n = scanner.nextInt();
-        //сам массив
-        int[] a=new int[n];
+        int[] a = new int[n];
+
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
-            System.out.println(a[i]);
         }
-
-        // тут ваше решение (реализуйте сортировку слиянием)
-        // https://ru.wikipedia.org/wiki/Сортировка_слиянием
-
-
-
-
-
-
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        mergeSort(a);
         return a;
     }
+
+     void mergeSort(int[] array) {
+
+        if (array.length < 2) return;
+
+        int[] left;
+        int[] right;
+        int size = array.length;
+        //Сценарий для массива нечетной размерности
+        if (size%2 != 0) {
+            left = new int[(size/2)+1];
+            right = new int[size/2];
+            System.arraycopy(array, 0, left, 0, (size/2)+1);
+            System.arraycopy(array, (size/2)+1, right, 0, size/2 );
+        }
+        //Сценарий для массива чётной размерности
+        else {
+            left = new int[size/2];
+            right = new int[size/2];
+            System.arraycopy(array, 0, left, 0, size/2);
+            System.arraycopy(array, size/2, right, 0, size/2);
+        }
+        //Рекурсивные вызовы для разбиения большого массива до состояния двух массивов с одним числом
+         mergeSort(left);
+         mergeSort(right);
+         //Слияние массивов
+         merge(array, left, right);
+     }
+
+     void merge (int[] array, int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        //Основной алгоритм сортировки
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k] = left[i];
+                i++;
+            }
+            else {
+                array[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        //На случай, если массив был нечетной размерности
+        while (i < left.length) {
+            array[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < right.length) {
+            array[k] = right[j];
+            j++;
+            k++;
+        }
+     }
+
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
         InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataB.txt");
