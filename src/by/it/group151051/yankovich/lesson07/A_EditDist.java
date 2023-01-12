9a69c9bab90523ded8538573e9ea5b89b21f23c0
Golovolastik <yankovich.aliaksei@gmail.com>
2023-01-12 16:21:12 +0300

    ready

--- src/by/it/group151051/yankovich/lesson07/A_EditDist.java
+++ src/by/it/group151051/yankovich/lesson07/A_EditDist.java
@@ -3,6 +3,7 @@ package by.it.group151051.yankovich.lesson07;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -42,11 +43,54 @@ public class A_EditDist {
 
     int getDistanceEdinting(String one, String two) {
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-        int result = 0;
+        int result = compute_Levenshtein_distanceDP(one, two);
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
+    }
+
+    static int compute_Levenshtein_distanceDP(String str1,
+                                              String str2)
+    {
+
+        // A 2-D matrix to store previously calculated answers
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+
+        for (int i = 0; i <= str1.length(); i++)
+        {
+            for (int j = 0; j <= str2.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                else if (j == 0) {
+                    dp[i][j] = i;
+                }
+
+                else {
+                    dp[i][j] = minm_edits(dp[i - 1][j - 1]
+                                    + NumOfReplacement(str1.charAt(i - 1),str2.charAt(j - 1)), // replace
+                            dp[i - 1][j] + 1, // delete
+                            dp[i][j - 1] + 1); // insert
+                }
+            }
+        }
+
+        return dp[str1.length()][str2.length()];
+    }
+
+    static int NumOfReplacement(char c1, char c2)
+    {
+        return c1 == c2 ? 0 : 1;
+    }
+
+    // receives the count of different
+    // operations performed and returns the
+    // minimum value among them.
+
+    static int minm_edits(int... nums)
+    {
+
+        return Arrays.stream(nums).min().orElse(
+                Integer.MAX_VALUE);
     }
 
 
