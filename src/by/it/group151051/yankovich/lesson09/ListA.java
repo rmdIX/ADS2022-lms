cbb59f988ab387e542a2c5533e8290711a5d604f
Golovolastik <yankovich.aliaksei@gmail.com>
2023-03-30 11:44:03 +0300

    fix: ListA default size

--- src/by/it/group151051/yankovich/lesson09/ListA.java
+++ src/by/it/group151051/yankovich/lesson09/ListA.java
@@ -1,9 +1,10 @@
 package by.it.group151051.yankovich.lesson09;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class ListA<T> {
-    private int size = 2;
+    private int size = 10;
     private T[] list;
     private int count = 0;
 
