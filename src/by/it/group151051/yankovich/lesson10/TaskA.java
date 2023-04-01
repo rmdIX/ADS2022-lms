eead1e7db67d6e67bbc91f9295b551766ac5f5dd
Golovolastik <yankovich.aliaksei@gmail.com>
2023-04-01 07:48:29 +0300

    feature: lesson10.1 complete

--- src/by/it/group151051/yankovich/lesson10/TaskA.java
+++ src/by/it/group151051/yankovich/lesson10/TaskA.java
@@ -10,28 +10,162 @@ public class TaskA<E extends Comparable<E>>  implements NavigableSet<E> {
     //в нем также может быть поле элемента E. Далее на этой основе ожидается бинарное дерево.
 
     //Обязательные к реализации методы и конструкторы
-    public TaskA() {
-    }
-    public class Node {
         public E value;
-        public Node left;
-        public Node right;
-    }
+        public TaskA left;
+        public TaskA right;
+
+        public TaskA(E value) {
+            this.value = value;
+        }
+
+        public TaskA() {
+            this(null);
+        }
+
+
+//    public TaskA() {
+//    }
 
         @Override
         public boolean add(E e) {
-            return false;
+            if (value == null){
+                value = e;
+                return true;
+            }
+            int cmp = e.compareTo(value);
+
+            if (cmp < 0) {
+                if (left == null) {
+                    left = new TaskA<>(e);
+                    return true;
+                } else {
+                    return left.add(e);
+                }
+            } else if (cmp > 0) {
+                if (right == null) {
+                    right = new TaskA<>(e);
+                    return true;
+                } else {
+                    return right.add(e);
+                }
+            } else {
+                return false;
+            }
         }
+
 
         @Override
         public boolean remove(Object o) {
-            return false;
+            if (!(o instanceof Comparable)) {
+                return false;
+            }
+            E value = (E) o;
+            TaskA<E> node = findNode(value, this);
+
+            if (node == null) {
+                return false;
+            }
+
+            if (node.left == null && node.right == null) {
+                replaceNode(node, null);
+            } else if (node.left == null) {
+                replaceNode(node, node.right);
+            } else if (node.right == null) {
+                replaceNode(node, node.left);
+            } else {
+                TaskA<E> minRightNode = findMinNode(node.right);
+                node.value = minRightNode.value;
+                replaceNode(minRightNode, minRightNode.right);
+            }
+
+            return true;
+        }
+
+    private TaskA<E> findNode(E value, TaskA<E> node) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = value.compareTo(node.value);
+
+        if (cmp < 0) {
+            return findNode(value, node.left);
+        } else if (cmp > 0) {
+            return findNode(value, node.right);
+        } else {
+            return node;
+        }
+    }
+
+    private TaskA<E> findMinNode(TaskA<E> node) {
+        if (node == null) {
+            return null;
+        }
+
+        if (node.left == null) {
+            return node;
+        }
+
+        return findMinNode(node.left);
+    }
+
+    private void replaceNode(TaskA<E> node, TaskA<E> replacement) {
+        if (node == this) {
+            this.value = replacement.value;
+            this.left = replacement.left;
+            this.right = replacement.right;
+            return;
+        }
+
+        TaskA<E> parent = findParent(node, this);
+
+        if (node == parent.left) {
+            parent.left = replacement;
+        } else {
+            parent.right = replacement;
+        }
+    }
+
+    private TaskA<E> findParent(TaskA<E> node, TaskA<E> parent) {
+        if (parent == null) {
+            return null;
+        }
+
+        if (node == parent.left || node == parent.right) {
+            return parent;
+        }
+
+        int cmp = node.value.compareTo(parent.value);
+
+        if (cmp < 0) {
+            return findParent(node, parent.left);
+        } else {
+            return findParent(node, parent.right);
+        }
         }
 
         @Override
         public String toString() {
-            return null;
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            toStringHelper(this, sb);
+            if (sb.length() > 1) {
+                sb.setLength(sb.length() - 2);
+            }
+            sb.append("]");
+            return sb.toString();
         }
+
+    private void toStringHelper(TaskA<E> node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
+
+        toStringHelper(node.left, sb);
+        sb.append(node.value.toString());
+        sb.append(", ");
+        toStringHelper(node.right, sb);
+    }
 
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
