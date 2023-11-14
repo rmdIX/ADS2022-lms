7763bfe837538f1e0dd80b5a77890eb36544380f
pay2joy2 <domoboi444@gmail.com>
2023-11-14 19:22:41 +0300

    Lesson 1 complited

--- src/by/it/group251051/smalianko/lesson01/FiboC.java
+++ src/by/it/group251051/smalianko/lesson01/FiboC.java
@@ -21,13 +21,54 @@ public class FiboC {
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
+    public static long pisano(long m)
+    {
+        long prev = 0;
+        long curr = 1;
+        long res = 0;
+
+        for(int i = 0; i < m * m; i++)
+        {
+            long temp = 0;
+            temp = curr;
+            curr = (prev + curr) % m;
+            prev = temp;
+
+            if (prev == 0 && curr == 1)
+                res= i + 1;
+        }
+        return res;
+    }
+
     long fasterC(long n, int m) {
+
         //Решение сложно найти интуитивно
         //возможно потребуется дополнительный поиск информации
         //см. период Пизано
-        return 0L;
-    }
+        // Getting the period
 
+        long pisanoPeriod = pisano(m);
+
+        n = n % pisanoPeriod;
+
+        long prev = 0;
+        long curr = 1;
+
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            long temp = 0;
+            temp = curr;
+            curr = (prev + curr) % m;
+            prev = temp;
+        }
+        return curr % m;
+        //return 0L;
+    }
 
 }
 
