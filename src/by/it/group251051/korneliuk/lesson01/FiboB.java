a3ac1f4841c467d241b332a8a781e861ddafe9d1
RandomNoName <Yan.Korneliuk@gmail.com>
2023-12-15 23:18:27 +0300

    Little fixes

--- src/by/it/group251051/korneliuk/lesson01/FiboB.java
+++ src/by/it/group251051/korneliuk/lesson01/FiboB.java
@@ -20,7 +20,7 @@ public class FiboB {
 
         //вычисление чисел простым быстрым методом
         FiboB fibo = new FiboB();
-        int n = 55555;
+        int n = -7;
         System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
     }
 
@@ -29,12 +29,15 @@ public class FiboB {
         ArrayList<BigInteger> arr = new ArrayList<BigInteger>();
         arr.add(BigInteger.valueOf(0));
         arr.add(BigInteger.valueOf(1));
-        for (int i = 1; i < n; i++){
+        for (int i = 1; i < Math.abs(n); i++) {
             arr.add(BigInteger.valueOf(0));
-            arr.set(2,arr.get(0).add(arr.get(1)));
+            arr.set(2, arr.get(0).add(arr.get(1)));
             arr.remove(0);
         }
-        return arr.get(arr.size()-1);
+        if (n > 0) return arr.get(arr.size()-1);
+        else {
+            return n % 2 == 0 ? arr.get(arr.size()-1) : arr.get(arr.size()-1).negate();
+        }
     }
 
 }
