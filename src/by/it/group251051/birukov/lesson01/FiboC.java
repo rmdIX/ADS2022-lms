ca49161e18d7dd6e6bea55979065cc6c6ba8552e
Birukov04 <andreybirukov04@mail.ru>
2023-12-20 17:17:41 +0300

    Lesson1 C исправлено

--- src/by/it/group251051/birukov/lesson01/FiboC.java
+++ src/by/it/group251051/birukov/lesson01/FiboC.java
@@ -28,18 +28,36 @@ public class FiboC {
         //возможно потребуется дополнительный поиск информации
         //см. период Пизано
 
-        int[] mas = new int[3];
-        mas[1]= 1;
-        for (int i=2; i<=n; i++) {
-            mas[2] = mas[1]+mas[0];
-            mas[0] = mas[1];
-            mas[1] = mas[2];
+        if (n<2) return n;
+        else {
+            long f, f1=1, f2=1, mod;
+            long[] mas_mod = new long[93];
+            mas_mod[1] = 1;
+            mas_mod[2] = 1;
+            int j=0, P_m=3, p=0, b=0, pos_mod;
+            for (int i = 3; i <= 92 && P_m != p; i++) {
+                f = f2 + f1;
+                f1 = f2;
+                f2 = f;
+                mas_mod[i]=f-f/m*m;
+                if (mas_mod[j] == mas_mod[i]) {
+                    if (b == 0) b = 1;
+                    p++;
+                    j++;
+                }
+                else {
+                    if (b == 1) {
+                        P_m += p;
+                        p = 0;
+                        j = 0;
+                    }
+                    P_m++;
+                }
+            }
+            pos_mod = (int) (n-n/P_m*P_m);
+            mod = mas_mod[pos_mod];
+            return mod;
         }
-        int f = mas[2], d;
-        d = f-f/m*m;
-        return d;
     }
-
-
 }
 
