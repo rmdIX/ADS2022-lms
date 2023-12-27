4c87d785a686d898e29b41f7c759e305db174557
Birukov04 <andreybirukov04@mail.ru>
2023-12-27 14:51:24 +0300

    Lesson6

---
+++ src/by/it/group251051/birukov/lesson06/C_LongNotUpSubSeq.java
@@ -0,0 +1,99 @@
+package by.it.group251051.birukov.lesson06;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Задача на программирование: наибольшая невозростающая подпоследовательность
+
+Дано:
+    целое число 1<=n<=1E5 ( ОБРАТИТЕ ВНИМАНИЕ НА РАЗМЕРНОСТЬ! )
+    массив A[1…n] натуральных чисел, не превосходящих 2E9.
+
+Необходимо:
+    Выведите максимальное 1<=k<=n, для которого гарантированно найдётся
+    подпоследовательность индексов i[1]<i[2]<…<i[k] <= длины k,
+    для которой каждый элемент A[i[k]] не больше любого предыдущего
+    т.е. для всех 1<=j<k, A[i[j]]>=A[i[j+1]].
+
+    В первой строке выведите её длину k,
+    во второй - её индексы i[1]<i[2]<…<i[k]
+    соблюдая A[i[1]]>=A[i[2]]>= ... >=A[i[n]].
+
+    (индекс начинается с 1)
+
+Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+
+    Sample Input:
+    5
+    5 3 4 4 2
+
+    Sample Output:
+    4
+    1 3 4 5
+*/
+
+
+public class C_LongNotUpSubSeq {
+
+    int getNotUpSeqSize(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //общая длина последовательности
+        int n = scanner.nextInt();
+        int[] m = new int[n];
+        //читаем всю последовательность
+        for (int i = 0; i < n; i++) {
+            m[i] = scanner.nextInt();
+        }
+        //тут реализуйте логику задачи методами динамического программирования (!!!)
+
+        int[] tail = new int[n];
+        int[] prev = new int[n];
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            int pos = lower_bound(m, tail, len, m[i]);
+            if (pos == len) {
+                ++len;
+            }
+            prev[i] = pos > 0 ? tail[pos - 1] : -1;
+            tail[pos] = i;
+        }
+        int[] res = new int[len];
+        for (int i = tail[len - 1]; i >= 0; i = prev[i]) {
+            res[--len] = i + 1;
+        }
+        for (int i = 0; i < res.length; i++)
+            System.out.print(res[i] + " ");
+        System.out.print("\n");
+        return res.length;
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+
+    static int lower_bound(int[] a, int[] tail, int len, int key) {
+        int lo = -1;
+        int hi = len;
+        while (hi - lo > 1) {
+            int mid = (lo + hi) >>> 1;
+            if (a[tail[mid]] >= key) {
+                lo = mid;
+            } else {
+                hi = mid;
+            }
+        }
+        return hi;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson06/dataC.txt");
+        C_LongNotUpSubSeq instance = new C_LongNotUpSubSeq();
+        int result = instance.getNotUpSeqSize(stream);
+        System.out.print(result);
+    }
+
+}
